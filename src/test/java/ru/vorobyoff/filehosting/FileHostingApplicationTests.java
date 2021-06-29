2a411dfe540c7c6c46eb1ca19ba04c466267d3c6
vorobyoff <>
2021-06-29 08:40:13 +0500

    Initial commit

---
+++ src/test/java/ru/vorobyoff/filehosting/FileHostingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vorobyoff.filehosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileHostingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
